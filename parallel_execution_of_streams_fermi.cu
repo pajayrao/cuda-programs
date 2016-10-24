8caf697b3f97fa340ff95bdca24c78075d1a714f
Ajay Rao <ajayraop3495@gmail.com>
2016-10-24 16:31:38 +0530

    Create parallel_execution_of_streams_fermi.cu

---
+++ parallel_execution_of_streams_fermi.cu
@@ -0,0 +1,67 @@
+/*
+ * Concurrent Kernels Execution for Fermi GPU
+ */
+
+#include<stdio.h>
+
+__global__ void kernel_1()
+{
+	double sum = 0.0;
+	for(int i=0;i<10;i++){
+		sum=sum+tan(0.1)*tan(0.1);
+
+	}
+}
+
+__global__ void kernel_2()
+{
+	double sum = 0.0;
+	for(int i=0;i<20;i++){
+		sum=sum+tan(0.2)*tan(0.2);
+
+	}
+}
+
+__global__ void kernel_3()
+{
+	double sum = 0.0;
+	for(int i=0;i<30;i++){
+		sum=sum+tan(0.3)*tan(0.3);
+
+	}
+}
+
+__global__ void kernel_4()
+{
+	double sum = 0.0;
+	for(int i=0;i<40;i++){
+		sum=sum+tan(0.4)*tan(0.4);
+
+	}
+}
+
+int main()
+{
+	int n_streams=5,i,j;
+	cudaStream_t *streams = (cudaStream_t*)malloc(n_streams*sizeof(cudaStream_t));
+
+	for(i=0;i<n_streams;i++)
+	{
+		cudaStreamCreate(&streams[i]);
+
+	}
+
+	dim3 block(1);
+	dim3 grid(1);
+
+	for(i=0;i<n_streams;i++)
+		kernel_1<<<grid,block,0,streams[i]>>>();
+	for(i=0;i<n_streams;i++)
+		kernel_2<<<grid,block,0,streams[i]>>>();
+	for(i=0;i<n_streams;i++)
+		kernel_3<<<grid,block,0,streams[i]>>>();
+	for(i=0;i<n_streams;i++)
+		kernel_4<<<grid,block,0,streams[i]>>>();
+
+	return 0;
+}
